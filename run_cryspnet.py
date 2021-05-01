e9e7ef771028de02861a519879a6ee79e0ae9aaa
Giselle Granchelli <giselle.granchelli@gmail.com>
2021-05-01 18:08:58 -0400

    clean up code

--- run_cryspnet.py
+++ run_cryspnet.py
@@ -4,35 +4,33 @@ from cryspnet.utils import topkacc
 from cryspnet.compare_structures import compare
 from pathlib import Path
 
-# required parameters ; only change these or the optional params
-ipt = "generate_structures/non-oxides.csv"
-output = 'generate_structures/version6/6-1/non-oxides/'
-run_version = "V6(non-oxides)"
+# required parameters ; only change these and/or the optional params
+ipt = "generate_structures/oxide_input.csv"
+output = 'generate_structures/test'
+run_version = "V6(oxides)"
 predictions = True
 crystals = True
 
 # Parameters for structure predictions
 # required params ; don't change
+
 pred_input = ipt
 pred_output = output
 
 # optional parameters
-# commented parameters will use defaults
-which_model = 'whole'  # 'whole', 'metal' or 'oxide'
-topn_bravais = 1
-topn_spacegroup = 1
-n_ensembler = 14  # max 14 models provided
-# batch_size =
-cpu = True
+# unincluded parameters will use default values set in CRYSPNet
+#model options: 'whole', 'metal' or 'oxide'
 if predictions is True:
-    make_predictions(input=pred_input, output=pred_output,
-                     which=which_model,
-                     topn_bravais=topn_bravais,
-                     topn_spacegroup=topn_spacegroup,
-                     n_ensembler=n_ensembler,
-                     # batch_size=batch_size,
-                     cpu=cpu,
-                     run_version=run_version)
+    pred_params = {}
+    with pred_params as pp:
+        pp['which'] = 'oxide'  # which model: 'whole', 'metal' or 'oxide'
+        pp['topn_bravais'] = 1
+        pp['topn_spacegroup'] = 1
+        pp['n_ensembler'] = 14  # max 14 models provided
+        pp['batch_size'] = 256 #default
+        pp['cpu'] = True
+
+    make_predictions(input=pred_input, output=pred_output, **pred_params, run_version=run_version)
 
 # Parameters for crystal generation
 # required parameters ; dont change
@@ -45,25 +43,15 @@ if crystals is True:
         crys_output = pred_output[:pred_output.rfind("//")]
 
     # optional parameters
-    # commented parameters will use defaults
-    topn_bravais = 1
-    topn_spacegroup = 1
-    n_workers = 4  # number of concurrent processes
-    n_trails = 10  # number of lattices to generate per composition
-    timeout = 300  # seconds before timing out of a process
-    formula_only = False
-    space_group_only = True
-    max_atoms = 500
+    crystal_params = {}
+    with crystal_params as cp:
+        cp['topn_bravais'] = 1
+        cp['topn_spacegroup'] = 1
+        cp['n_workers'] = 4  # number of concurrent processes
+        cp['n_trails'] = 10  # number of lattices to generate per composition
+        cp['timeout'] = 300  # seconds before timing out of a process
+        cp['formula_only'] = False
+        cp['space_group_only'] = True
+        cp['max_atoms'] = 500
 
-    generate_crystals(input=crys_input, output=crys_output,
-                      # error=error,
-                      topn_bravais=topn_bravais,
-                      topn_spacegroup=topn_spacegroup,
-                      n_workers=n_workers,
-                      n_trails=n_trails,
-                      timeout=timeout,
-                      formula_only=formula_only,
-                      space_group_only=space_group_only,
-                      max_atoms=max_atoms,
-                      run_version=run_version
-                      )
+    generate_crystals(input=crys_input, output=crys_output, **crystal_params, run_version=run_version)
